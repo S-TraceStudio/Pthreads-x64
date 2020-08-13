2368ad994d04a7a9a6403327b844ddc8b5ab8e61
Franck Chateau <franck@s-trace.com>
2020-08-13 14:00:31 +0200

    allowing public read permision

--- scripts/compressAndUploadPthreads.bat
+++ scripts/compressAndUploadPthreads.bat
@@ -13,6 +13,9 @@ del /F /Q Pthread-x64.zip
 :: upload to s3 bucker
 aws s3 cp Pthread-x64.zip s3://s-trace.dependencies/
 
+:: give public READ permission
+aws --endpoint-url https://s3-ap-southeast-1.amazonaws.com/ s3api put-object-acl --bucket s-trace.dependencies --key Pthread-x64.zip --acl public-read
+
 ::
 cd ..
 
